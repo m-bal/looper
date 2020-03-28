b7f4b2f801ed072feacd0d122da473e8325707ed
m-bal <mbal2@csub.edu>
2020-03-28 14:25:28 -0700

    added coded for looping 1 section

--- looper.js
+++ looper.js
@@ -1,13 +1,17 @@
 //ToDo:
 //* read the area that changes the video time
 //* see if you can manipulate that time.  
-youtubeTime();
 
-function youtubeTime(){
 
-	while(1){
-		var progressbar = document.getElementsByClassName("ytp-progress-bar").item(0);
-		console.log(progressbar.getAttribute("aria-valuetext"))
-		
+ytplayer = document.getElementsByClassName('video-stream')[0];
+
+function youtubeTime(event){
+	
+	if(ytplayer.currentTime < 10){
+		ytplayer.currentTime = 10;
+	}
+	if(ytplayer.currentTime > (3*60)+8){
+		ytplayer.currentTime = 10;
 	}
 }
+ytplayer.addEventListener('timeupdate', youtubeTime);
