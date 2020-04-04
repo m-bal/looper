9c0ac25d5eb2435cbbbaf58bdf3afffc3010d0b1
m-bal <mbal2@csub.edu>
2020-04-04 04:39:23 -0700

    added code to allow for one time loop

--- background.js
+++ background.js
@@ -1,18 +1,9 @@
 console.log('Starting Background Script');
-browserAction.onClicked.addListener(modifyTimes);
 
-var start_time;
-var end_time;
-
-start_time = 10;
-
-function modifyTimes(){
-    var x = document.getElementById("frm_times");
-    start_time = x.elements[0].value;
-	end_time = x.elements[1].value;
-	console.log("Printing out start time");
-	console.log(start_time);
-    document.getElementById("demo").innerHTML = start_time + end_time;
-}
-
-//console.log(start_time);
+// function handleMessage(request, sender, sendResponse) {
+// 	console.log("Message from the content script: " +
+// 	  request.startTime);
+// 	//sendResponse({response: "Response from background script"});
+// }
+  
+// browser.runtime.onMessage.addListener(handleMessage);
--- looper.js
+++ looper.js
@@ -18,12 +18,17 @@ function youtubeTime(event){
 	}
 }
 
-function modifyTimes(){
-    var x = document.getElementById("frm_times");
-    start_time = x.elements[0].value;
-	end_time = x.elements[1].value;
-	console.log("Printing out start time");
-	console.log(start_time);
-    document.getElementById("demo").innerHTML = start_time + end_time;
+
+function handleMessage(request, sender, sendResponse) {
+	console.log("Message from the content script: " +
+	  request.startTime);
+	start_time = request.startTime;
+	end_time = request.endTime;
+	sendResponse({success: true});
 }
+  
+chrome.runtime.onMessage.addListener(handleMessage);
+
+
+
 ytplayer.addEventListener('timeupdate', youtubeTime);
